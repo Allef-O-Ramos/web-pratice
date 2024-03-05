fb40cc46413da63f2d41a54ce85bc352064a1b9b
allef <aor.eng20@uea.edu.br>
2024-03-05 10:41:01 -0400

    first commit

---
+++ discover-rocketseat/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  /*if (html.classList.contains("light")) {
+    html.classList.remove("light");
+  } else {
+    html.classList.add("light");
+  }*/
+  html.classList.toggle("light")
+
+  const image = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    image.setAttribute("src", "./assets/profile1.png")
+  } else {
+    image.setAttribute("src", "./assets/profile.png")
+  }
+}
